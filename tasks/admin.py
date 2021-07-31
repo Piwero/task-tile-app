6f221e99981d16507aa1205b6ea3f676fc7b6383
Piwero <piwero@gmail.com>
2021-07-31 18:57:30 +0100

    create models and admin for Tile and tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Task,
+    Tile,
+)
+
+
+@admin.register(Tile)
+class TileAdmin(admin.ModelAdmin):
+    model = Tile
+    list_display = ["id", "launch_date", "status"]
+    list_filter = ["launch_date", "status"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+    list_display = [
+        "title",
+        "order",
+        "description",
+        "tile",
+        "type",
+        "user",
+    ]
+    list_filter = ["order", "type", "tile", "user"]
+    readonly_fields = ["created_at", "updated_at"]
